49bbf927e5966d18ebecbd9c03fe8b94934ab337
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-03-25 20:00:39 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,48 @@
-console.log("Hello via Bun!");
+/*
+ * Given a string, returns the length 
+ * of the largest run in the string.
+ *
+ * @author  Curtis Edwards
+ * @version 1.0
+ * @since   2024-03-25
+ */
+
+import { createPrompt } from 'bun-promptx'
+
+
+function maxRun(string) {
+  /*
+   * this function calculates the max run of a string
+   */
+  let maxRun = 1
+  let preivousChar = ""
+
+  for (let char of string) {
+    let currentRun = 1
+    if (preivousChar = char) {
+      currentRun++
+      if (currentRun > maxRun) {
+        maxRun = currentRun
+      }
+    }
+    let preivousChar = char
+
+  return maxRun
+}
+
+
+// input
+const userString = createPrompt("Enter a string: ")
+const string = parseFloat(userString.value || "-1")
+
+// error check
+if (string < 0) {
+  console.log("Invalid input.")
+} else {
+  // process
+  let maxRun = maxRun(string)
+  // output
+  console.log(`The string ${string} has a max run of ${maxRun}!`)
+}
+
+console.log("\nDone.")
